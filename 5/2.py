9e05daf5479c44a603a7cd7a1c1c4bcdc32f827a
Aleksandr Sofronov <42114787+Ynxela@users.noreply.github.com>
2019-05-17 11:59:30 +0300

    Доделал 2 задачу

--- 5/2.py
+++ 5/2.py
@@ -1,29 +1,54 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
 # Написать программу сложения и умножения двух шестнадцатеричных чисел. При этом каждое
 # число представляется как массив, элементы которого это цифры числа.
 # Например, пользователь ввёл A2 и C4F. Сохранить их как [‘A’, ‘2’] и [‘C’, ‘4’, ‘F’] соответственно.
 # Сумма чисел из примера: [‘C’, ‘F’, ‘1’], произведение - [‘7’, ‘C’, ‘9’, ‘F’, ‘E’].
 
-from collections import deque
+from collections import OrderedDict
 
 
 class SixteenNums():
 
     def __init__(self, number):
         self.number = list(number.upper())
-        self.num_len = len(number)
 
     def __add__(self, second_num):
-        c = None
-        deq = deque([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'])
-        second_num_len = len(second_num)
+        weights = OrderedDict({'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, 'A': 10,
+                               'B': 11, 'C': 12, 'D': 13, 'E': 14, 'F': 15, '10': 'A', '11': 'B', '12': 'C', '13': 'D',
+                               '14': 'E', '15': 'F'})
+        tmp = 0
+        result_num = []
+        result = ''
 
-        if self.num_len > second_num_len:
-            c = self.number.copy()
+        # Создаем вспомогательные списки на основе имеющихся объектов
+        if len(self) > len(second_num):
+            c = self.reverse()
+            d = second_num.reverse()
         else:
-            c = second_num.copy()
+            c = second_num.reverse()
+            d = self.reverse()
 
-        for num in range(0, len(c), -1):
-            pass
+        for num in range(0, len(c)):
+            try:
+                res = weights[c[num]] + weights[d[num]] + tmp
+            except IndexError:
+                res = weights[c[num]] + tmp
+            if res > 15:
+                res -= 16
+                tmp = 1
+            else:
+                tmp = 0
+            result_num.append(str(weights[str(res)]))
+            if num == len(c) - 1 and tmp == 1:
+                result_num.append(str(tmp))
+
+        result_num = result_num[::-1]
+
+        for num in result_num:
+            result += num
+
+        return SixteenNums(result)
 
     def __str__(self):
         return str(self.number)
@@ -31,12 +56,12 @@ class SixteenNums():
     def __len__(self):
         return len(self.number)
 
-    def copy(self):
-        return self.number
+    def reverse(self):
+        return self.number[::-1]
+
 
 a = SixteenNums(input('Введите 1 16-ричное число: '))
 b = SixteenNums(input('Введите 2 16-ричное число: '))
 
-print(a, b)
-
-print(a + b)
+print('\na + b =', a + b, '\n')
+print('a + b + a + b =', a + b + a + b)
